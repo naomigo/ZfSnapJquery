603ac667b1cb7799dbc1961fe73930c31209326a
naomigo <knome2001@hotmail.com>
2014-05-02 10:56:04 +0100

    Datetimepicker.php

---
+++ src/ZfSnapJquery/Form/Element/Datetimepicker.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace ZfSnapJquery\Form\Element;
+
+use Zend\Form\Element;
+
+/**
+ * Datepicker
+ *
+ * @author Naomi Gotts <knome2001@hotmail.com>
+ * @copyright 2014 (c)
+ */
+class Datetimepicker extends Element
+{
+    /**
+     * Seed attributes
+     *
+     * @var array
+     */
+    protected $attributes = array(
+        'type' => 'datetimepicker'
+    );
+}
+?>

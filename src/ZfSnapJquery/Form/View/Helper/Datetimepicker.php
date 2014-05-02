a6adb4a2a78dd18e70d58e66897cc829e081041e
naomigo <knome2001@hotmail.com>
2014-05-02 10:58:00 +0100

    Create Datetimepicker.php

---
+++ src/ZfSnapJquery/Form/View/Helper/Datetimepicker.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace ZfSnapJquery\Form\View\Helper;
+
+/**
+ * Datepicker
+ *
+ * @author Naomi Gotts <knome2001@hotmail.com>
+ * @copyright 2014 (c)
+ */
+class Datetimepicker extends AbstractSimpleHelper
+{
+    /**
+     * @return string
+     */
+    public function getCallerMethodName()
+    {
+        return 'datetimepicker';
+    }
+}
+?>

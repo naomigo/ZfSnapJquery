83d850e6e1e3dcc5ad40b764476f5f146a10416d
naomigo <knome2001@hotmail.com>
2014-05-02 10:58:25 +0100

    Create Timepicker.php

---
+++ src/ZfSnapJquery/Form/View/Helper/Timepicker.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace ZfSnapJquery\Form\View\Helper;
+
+/**
+ * Datepicker
+ *
+ * @author Naomi Gotts <knome2001@hotmail.com>
+ * @copyright 2014 (c)
+ */
+class Timepicker extends AbstractSimpleHelper
+{
+    /**
+     * @return string
+     */
+    public function getCallerMethodName()
+    {
+        return 'timepicker';
+    }
+}
+?>

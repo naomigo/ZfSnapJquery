f14f1a860e60599302fc5c9873f649ddd99f8e5d
naomigo <knome2001@hotmail.com>
2014-05-02 10:56:32 +0100

    Create Timepicker.php

---
+++ src/ZfSnapJquery/Form/Element/Timepicker.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace ZfSnapJquery\Form\Element;
+
+use Zend\Form\Element;
+
+/**
+ * Timepicker
+ *
+ * @author Naomi Gotts <knome2001@hotmail.com>
+ * @copyright 2014 (c)
+ */
+class Timepicker extends Element
+{
+    /**
+     * Seed attributes
+     *
+     * @var array
+     */
+    protected $attributes = array(
+        'type' => 'timepicker'
+    );
+}
+?>
